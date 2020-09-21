000fc1d31f4036c2a16e1ed5e593aee0501476fa
carloszimm <carloszimmerle@gmail.com>
2020-09-21 18:25:24 -0300

    feat: add node-config to project

--- routes/health.js
+++ routes/health.js
@@ -1,62 +1,66 @@
-const router = require('express').Router();
-const async = require('async');
-var fetch = require('node-fetch');
+const router = require('express').Router(),
+  async = require('async'),
+  fetch = require('node-fetch'),
+  config = require('config');
+
+const createMessage = (status, name, url, message = "") =>
+  ({
+    "ok": status,
+    "name": name,
+    "error": message,
+    "details": {
+      "url": url
+    }
+  });
 
 router.get('/', (req, res) => {
-  function createMessage(status, name, url, message = "") {
-    return {
-      "ok": status,
-      "name": name,
-      "error": message,
-      "details": {
-        "url": url
-      }
-    }
-  }
 
-  let ret = {
-    "ok": true
-  };
+  let status = true;
 
   async.parallel([
     function (callback) {
-      let url = "http://prometheus:9090/-/healthy";
+      let url =
+        `http://${config.get('prometheus.server')}:${config.get('prometheus.port')}` +
+        `${config.get('prometheus.health')}`;
 
       fetch(url)
         .then(res => {
           if (res.ok) {
             callback(null, createMessage(true, 'prometheus', url));
           } else {
-            ret.ok = false;
+            status = false;
             callback(null, createMessage(false, 'prometheus', url, res.statusText));
           }
         })
         .catch(err => {
-          ret.ok = false;
+          status = false;
           callback(null, createMessage(false, 'prometheus', url, err.message));
         });
     },
     function (callback) {
-      let url = "http://elasticsearch:9200/_cat/health?format=JSON";
+      let url = `http://${config.get('elastic.server')}:${config.get('elastic.port')}` +
+        `${config.get('elastic.health')}?format=JSON`;
 
       fetch(url)
         .then(res => {
           if (res.ok) {
             callback(null, createMessage(true, 'elasticsearch', url));
           } else {
-            ret.ok = false;
+            status = false;
             callback(null, createMessage(false, 'elasticsearch', url, res.statusText));
           }
         })
         .catch(err => {
-          ret.ok = false;
+          status = false;
           callback(null, createMessage(false, 'elasticsearch', url, err.message))
         });
     }
   ], function (err, checks) {
-    ret.checks = checks;
     res.status = 201;
-    res.json(ret);
+    res.json({
+      ok: status,
+      checks: checks
+    });
   });
 
 
