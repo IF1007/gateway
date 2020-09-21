c871e602144f5031531baee37079d8c254d0ba38
carloszimm <carloszimmerle@gmail.com>
2020-09-21 17:59:10 -0300

    fix: adjust indentation

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const app = require('express')()
 
 // handle favicon
-app.use(function(request, response, next){
-    if(request.url === 'favicon.ico'){
-        response.writeHead(200, {'Content-Type': 'image/x-icon'});
-        response.end('');
-    } else{
-        next();
-    }
+app.use(function (request, response, next) {
+  if (request.url === 'favicon.ico') {
+    response.writeHead(200, { 'Content-Type': 'image/x-icon' });
+    response.end('');
+  } else {
+    next();
+  }
 });
 
 //routes
 app.use('/', require('./routes'));
 
 // error handling
-app.use(function(request, response, next){
-    let err = new Error('Not Found');
-    err.status = 404;
+app.use(function (request, response, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
 
-    next(err);
+  next(err);
 });
-app.use(function(err, request, response, next){
-    response.status(err.status || 500).json({err: err.message});
+app.use(function (err, request, response, next) {
+  response.status(err.status || 500).json({ err: err.message });
 });
 
 // listener
--- bin/www/index.js
+++ bin/www/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 var app = require('../../app')
-var server = app.listen(8081, function(){
-    let host = server.address().address;
-    let port = server.address().port;
+var server = app.listen(8081, function () {
+  let host = server.address().address;
+  let port = server.address().port;
 
-    console.log(`Gateway listening at http://${host}:${port}`)
+  console.log(`Gateway listening at http://${host}:${port}`)
 });
