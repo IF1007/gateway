6628bf0200d280cc87e6dcf154354bcb45ce2bd0
carloszimm <carloszimmerle@gmail.com>
2020-09-28 16:38:00 -0300

    refactor: readd dimensions endpoints

--- routes/metrics.js
+++ routes/metrics.js
@@ -4,10 +4,10 @@ const PrometheusService = require('../services/prometheusService');
 const metricsController = require('../controllers/metricsController')(new PrometheusService());
 
 router.get('/', metricsController.getMetrics.bind(metricsController));
-router.get('/:_metric', metricsController.getMetric.bind(metricsController));
+router.get('/m/:_metric', metricsController.getMetric.bind(metricsController));
 
-//router.get('/dimensions', metricsController.getDimensions.bind(metricsController));
-//router.get('/dimensions/:_dimension', metricsController.getDimension.bind(metricsController));
+router.get('/ds', metricsController.getDimensions.bind(metricsController));
+router.get('/ds/:_dimension', metricsController.getDimension.bind(metricsController));
 
 
 module.exports = router;
