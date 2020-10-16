2bdce4d12cd3bf4d519ae4fc59656d2eef93bce8
carloszimm <carloszimmerle@gmail.com>
2020-10-16 16:51:51 -0300

    refactor: change graphql settings to routes

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const app = require('express')(),
-  bodyParser = require('body-parser'),
-  { graphqlHTTP } = require('express-graphql'),
-  config = require('config');
-
-const { schema } =
-  require('prom-graphql')(`http://${config.get('prometheus.server')}:${config.get('prometheus.port')}`);
+  bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,13 +16,6 @@ app.use(function (request, response, next) {
 
 // routes
 app.use('/', require('./routes'));
-
-// graphql
-// activates graphiql only when the DEBUG env variable is set
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: !!process.env.DEBUG
-}));
 
 // error handling
 app.use(function (request, response, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-const router = require('express').Router();
+const router = require('express').Router(),
+  { graphqlHTTP } = require('express-graphql'),
+  config = require('config');
+
+const { schema } =
+  require('prom-graphql')(`http://${config.get('prometheus.server')}:${config.get('prometheus.port')}`);
 
 router.use('/metrics', require('./metrics'));
-
-router.use('/logs', require('./logs'))
-
+router.use('/logs', require('./logs'));
 router.use('/check', require('./healthCheck'));
+
+// graphql
+// activates GraphiQL(UI) only when the DEBUG env variable is set
+router.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: !!process.env.DEBUG
+}));
 
 module.exports = router;
