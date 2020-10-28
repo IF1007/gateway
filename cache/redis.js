65f0bdb6454d77f58f7778342bf7ef7dcdf46689
carloszimm <carloszimmerle@gmail.com>
2020-10-28 16:28:15 -0300

    style: update indentation and missing semi colons

--- cache/redis.js
+++ cache/redis.js
@@ -1,27 +1,29 @@
-const redis = require('redis')
-const config = require('config')
-const client = redis.createClient(config.get('redis.port'), config.get('redis.server'))
+const redis = require('redis'),
+  config = require('config');
 
-client.on("error", function(error) {
-    console.error(error);
-  });
-  
-const getFromRedis = (key) => {
-    return new Promise((resolve, reject) => {
-        client.get(key, (err, value) => {
-            if(err){
-                reject(err);
-            }else{
-                console.log("requested cache!")
-                resolve(JSON.parse(value));
-            }
-        });
+const client =
+  redis.createClient(config.get('redis.port'), config.get('redis.server'));
+
+client.on("error", function (error) {
+  console.error(error);
+});
+
+const getFromRedis = key => {
+  return new Promise((resolve, reject) => {
+    client.get(key, (err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log("requested cache!")
+        resolve(JSON.parse(value));
+      }
     });
+  });
 }
 
 const updateRedis = (key, value) => {
-        client.set(key, JSON.stringify(value))
-        client.expire(key, config.get('redis.expire'))
+  client.set(key, JSON.stringify(value));
+  client.expire(key, config.get('redis.expire'));
 }
 
-module.exports = {getFromRedis, updateRedis}
+module.exports = { getFromRedis, updateRedis };
